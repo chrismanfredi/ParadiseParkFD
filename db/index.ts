48dd5e792fd7e36b5ba8604540264368601b2273
Chris <chrismanfredi11@gmail.com>
2025-11-09 09:38:16 -0500

    11/9 5

--- db/index.ts
+++ db/index.ts
@@ -7,13 +7,8 @@ import * as schema from "./schema";
 type DrizzleDb = NodePgDatabase<typeof schema>;
 
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace NodeJS {
-    interface Global {
-      __drizzlePgPool?: Pool;
-      __drizzleDb?: DrizzleDb;
-    }
-  }
+  const __drizzlePgPool: Pool | undefined;
+  const __drizzleDb: DrizzleDb | undefined;
 }
 
 const connectionString = process.env.SUPABASE_DB_URL;
@@ -22,18 +17,23 @@ if (!connectionString) {
   throw new Error("SUPABASE_DB_URL is not set. Please update your .env file.");
 }
 
+const globalWithDrizzle = globalThis as typeof globalThis & {
+  __drizzlePgPool?: Pool;
+  __drizzleDb?: DrizzleDb;
+};
+
 const pool =
-  globalThis.__drizzlePgPool ??
+  globalWithDrizzle.__drizzlePgPool ??
   new Pool({
     connectionString,
     max: 5,
   });
 
-const db: DrizzleDb = globalThis.__drizzleDb ?? drizzle(pool, { schema });
+const db: DrizzleDb = globalWithDrizzle.__drizzleDb ?? drizzle(pool, { schema });
 
 if (process.env.NODE_ENV !== "production") {
-  globalThis.__drizzlePgPool = pool;
-  globalThis.__drizzleDb = db;
+  globalWithDrizzle.__drizzlePgPool = pool;
+  globalWithDrizzle.__drizzleDb = db;
 }
 
 export { db, pool, schema };
